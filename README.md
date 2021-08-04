37a6f047e4b06eb422519e2bddfdc87066bd3eb1
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-08-04 11:31:14 +0200

    Initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Noticeable Angular Sample
+
+This project shows how to integrate Noticeable Widgets in an Angular Web app.
+
+We suggest looking at the commits to see a comprehensive list of the changes:
+https://github.com/noticeable-app/noticeable-sample-angular/commits/master
+
+You can also run `ng serve` and navigate to `http://localhost:4200/` in order to see a live sample.
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: Page1Component},
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component},
+  {path: 'page3', component: Page3Component},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
